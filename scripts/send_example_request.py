a2f1956ac119fe9c915ab0fa4b86b8a9ee336624
Piotr Szefer <szefer85@gmail.com>
2024-12-22 09:45:44 +0100

    Sending an example request WORKS

--- scripts/send_example_request.py
+++ scripts/send_example_request.py
@@ -1,15 +1,17 @@
 import requests
 
-data = {
-    "query1": "Markov Chain Monte Carlo",
-    "query2": "Markov Chain Monte Carlo",
+query = {
+    "query": "Markov Chain Monte Carlo",
 }
+
+
+# query = "Markov Chain Monte Carlo"
 
 headers = {
     "Content-Type": "application/json",
 }
 
 url = "http://localhost:8080/find_similar_abstracts"
-response = requests.post(url=url, headers=headers, json=data)
+response = requests.post(url=url, headers=headers, json=query)
 print(f"Recieved status code {response.status_code}")
 print(f"Recieved {response.json()}")
